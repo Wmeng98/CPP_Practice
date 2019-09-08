261906aae8a4e02ca419916708796814a18ecce3
Wilson Meng <w5meng@uwaterloo.ca>
2019-09-08 16:54:59 -0400

    bazel-tutorial first commit

---
+++ bazel-tutorial/tut1/main/src/main.cpp
@@ -0,0 +1,17 @@
+//#include "lib/hello-time.h"
+//#include "main/hello-greet.h"
+#include <iostream>
+#include <string>
+
+// char** is an array of array of char OR array of strings
+int main(int argc, char** argv) {
+//    std::string who = "world";
+//    if (argc > 1) {
+//        who = argv[1];
+//    }
+//    std::cout << get_greet(who) << std::endl;
+//    print_localtime();
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
